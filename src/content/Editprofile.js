a305640957e030d4d90ea0fab8edbd07fea9a981
KUNKANIT THONGKENG <92772306+Imfirn@users.noreply.github.com>
2022-04-16 02:52:29 +0700

    profile

--- src/content/Editprofile.js
+++ src/content/Editprofile.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Editprofile({changeTofalse}) {
+function Editprofile({ changeTofalse }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  console.log(lastName);
+  const onsubmit = () => {};
+
   return (
     <div>
-        <div style={{cursor:"pointer"}} onClick={()=>changeTofalse()}>go back</div>
-        <input value="First name"/>
-        <input value="last name"/>
-        <button>submit</button>
-
+      <div style={{ cursor: "pointer" }} onClick={() => changeTofalse()}>
+        go back
+      </div>
+      <div className="grid grid-cols-2 gap-7">
+        <div className="font-semibold text-[#E54E3D]">
+          <h5>ชื่อ</h5>
+        </div>
+        <input
+          className="bg-violet-100"
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-7 mt-2">
+        <div className="font-semibold text-[#E54E3D]">
+          <h5>นามสกุล</h5>
+        </div>
+        <input
+          className="bg-violet-100"
+          value={lastName}
+          onChange={(event) => setlastName(event.target.value)}
+        />
+        <button onClick={onsubmit}>submit</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Editprofile
+export default Editprofile;
--- src/content/Profile.js
+++ src/content/Profile.js
@@ -3,10 +3,10 @@ import React from 'react'
 function Profile() {
   return (
       <>
-    <div className='grid grid-cols-3 gap-9 bg-slate-400  '>
+    <div className='grid grid-cols-3 gap-9   '>
         <h1 className='mb-5  text-2xl font-semibold	 '>ข้อมูลส่วนตัว</h1>
         <div className='col-span-2  text-right m-2'>
-        <input type="submit" className='text-right bg-red-500' value="แก้ไขข้อมูล"/>
+        
         
         </div>
         
@@ -16,7 +16,7 @@ function Profile() {
                 <h5>ชื่อ</h5>
             </div>
             <div className=''>
-                <h5>f</h5>
+                <h5>สมศรี</h5>
             </div>
         </div>
         <div className='grid grid-cols-2 gap-7 ทะ-2'>
@@ -24,7 +24,7 @@ function Profile() {
                 <h5>นามสกุล</h5>
             </div>
             <div className=''>
-                <h5>f</h5>
+                <h5>แสงสว่าง</h5>
             </div>
         </div>
         
